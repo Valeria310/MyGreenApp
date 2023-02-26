ef9da0d82ca29a0e98af0b1077fe2b56edace590
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-02-26 16:15:07 +0300

    feat: add Ticker component and styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import NavSection from '../NavSection';
 import RecycleSection from '../RecycleSection';
+import { Ticker } from '../Ticker/Ticker';
 
 import './App.scss';
 
 export const App = () => (
   <div className="App">
     <NavSection />
+    <Ticker />
     <RecycleSection />
   </div>
 );
