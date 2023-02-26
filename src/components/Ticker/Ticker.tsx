ef8d07a4804236e82c7268ca185deb0a1bca39e3
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-02-26 21:34:50 +0300

    fix: fix Ticker component

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Marquee from 'react-double-marquee';
+import Marquee from "react-fast-marquee";
 import c from './Ticker.module.scss';
 
 export const Ticker = () => (
     <div className={c.container}>
         <section className={c.ticker}>
-            <Marquee className={c.aaa} speed="0.15" delay="0" direction="left" childMargin="0">
+            <Marquee speed={200} gradient={false}>
                 <span className={c.text}> <span className={c.text__bold}>присоединяйся</span> к нашему <span className={c.text__italic_bold}> эко-движению!</span></span>
                 <span className={c.text}> <span className={c.text__bold}>присоединяйся</span> к нашему <span className={c.text__italic_bold}> эко-движению!</span></span>
             </Marquee>
