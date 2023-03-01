ab17143165b20773c3eb2e8ee49a063d02ac586b
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-03-01 11:16:12 +0300

    feat: add Header and Parallax components, styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,15 @@ import NavSection from '../NavSection';
 import RecycleSection from '../RecycleSection';
 import { Ticker } from '../Ticker/Ticker';
 import { Footer } from './../Footer/Footer';
+import { ParallaxSection } from './../Parallax/Parallax';
+import { Header } from './../Header/Header';
 
 import './App.scss';
 
 export const App = () => (
   <div className="App">
+    <Header />
+    <ParallaxSection />
     <NavSection />
     <Ticker />
     <RecycleSection />
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,8 +3,8 @@ import './Navigation.scss';
 
 export const Navigation = () => (
     <nav className='navigation'>
-        <a href="#" className='navigation_link'>Куда сдать</a>
+        <a href="#map" className='navigation_link'>Куда сдать</a>
         <a href="#recycle" className='navigation_link'>Что перерабатываем</a>
-        <a href="#" className='navigation_link'>О нас</a>
+        <a href="#about" className='navigation_link'>О нас</a>
     </nav>
 )
