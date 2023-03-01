748765174b0e858f1bdc1b8a9e44adbde2b2ff65
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-03-01 11:18:24 +0300

    fix: upfate footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ import { Navigation } from '../Navigation/Navigation';
 export const Footer = () => (
     <footer className="footer">
         <div className="container footer-container">
-            <a href='#' className="footer-logo"></a>
+            <a href='#about' className="footer-logo"></a>
             <Navigation />
             <span className="socials">mygreen@mygreen.by</span>
         </div>
-        <a href="#" className='up-btn'></a>
+        <a href="#about" className='up-btn'></a>
     </footer>
 )
