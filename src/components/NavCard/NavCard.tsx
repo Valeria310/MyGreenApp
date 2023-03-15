99e6b1f150d0949585e77961e277e8a3abe9383a
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2023-03-15 11:43:02 +0300

    feat: new design for NavSection

--- src/components/NavCard/NavCard.tsx
+++ src/components/NavCard/NavCard.tsx
@@ -2,17 +2,26 @@ import { FC } from 'react';
 import { NavCardProps } from './NavCardProps';
 
 import './NavCard.scss';
+import NavArrow from '../../assets/icons/nav-arrow.svg';
 
 export const NavCard: FC<NavCardProps> = ({card}) => (
     <div className="nav-card">
+        <div className="nav-card__content">
+            <h2 className="nav-card__heading">
+                {card.heading}
+            </h2>
+            <div className="nav-card__text">
+                { card.content.map(content => <p key={content.id}>{ content.text }</p>) }
+            </div>
+            <div className="nav-card__button">
+                <a href={card.href}>
+                    <span>{card.buttonValue}</span>
+                    <img src={NavArrow} alt='arrow' />
+                </a>
+            </div>
+        </div>
         <div className="nav-card__image">
             <img src={card.image} alt="card" />
         </div>
-        <div className="nav-card__text">
-            {card.text}
-        </div>
-        <a href={card.href} className="button nav-card__button">
-            {card.buttonValue}
-        </a>
     </div>
 )
