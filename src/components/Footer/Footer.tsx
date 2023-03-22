a5a7f5ea8a11358df2f39a1fec0535ff24e7a21d
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-03-22 13:49:35 +0300

    feat: update Footer design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,14 @@ import { Navigation } from '../Navigation/Navigation';
 
 export const Footer = () => (
     <footer className="footer">
+        <div className="hill"></div>
+        <div className="small_trees"></div>
+        <div className="darh_hill"></div>
+        <div className="big_trees"></div>
         <div className="container footer-container">
             <a href='#about' className="footer-logo"></a>
             <Navigation />
-            <span className="socials">mygreen@mygreen.by</span>
+            <span className="socials">mygreenfeedback@gmail.com</span>
         </div>
         <a href="#about" className='up-btn'></a>
     </footer>
