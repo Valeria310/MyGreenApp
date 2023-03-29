9ca57246fd4eaf5521b00c7d3a0ba0480d41d02b
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-03-29 09:34:37 +0300

    fix: fix Footer component and styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,10 @@ import { Navigation } from '../Navigation/Navigation';
 
 export const Footer = () => (
     <footer className="footer">
-        <div className="hill"></div>
-        <div className="small_trees"></div>
-        <div className="darh_hill"></div>
-        <div className="big_trees"></div>
+        <div className="footer_hill"></div>
+        <div className="footer_small_trees"></div>
+        <div className="footer_dark_hill"></div>
+        <div className="footer_big_trees"></div>
         <div className="container footer-container">
             <a href='#about' className="footer-logo"></a>
             <Navigation />
