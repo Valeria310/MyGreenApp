078feba8e7c991682aaa16f6910049fa906e88d3
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2023-09-20 23:03:02 +0300

    feat: edit logics for RecyclePage

--- src/components/RecycleCard/RecycleCard.tsx
+++ src/components/RecycleCard/RecycleCard.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import RecycleCardButton from './Button';
 import RecycleCardContent from './Content';
@@ -15,26 +15,38 @@ export const RecycleCard: FC<RecycleCardProps> = ({ card, expandable }) => {
     const [isExpanded, setExpanded] = useState(false);
     const { content } = card;
     const navigate = useNavigate();
+    const { state } = useLocation();
 
-    const handleCardClick = () => {
+    useEffect(() => {
+        if (state && card.heading === state) {
+            setExpanded(true);
+            const currentCard = document.getElementById(card.heading);
+            if (currentCard) {
+                currentCard.scrollIntoView({ behavior: 'smooth' });
+            }
+            window.history.replaceState(null, state);
+        }
+    }, []);
+
+    const handleCardClick = (heading: string) => {
         if (expandable) {
             setExpanded(!isExpanded);
         } else {
-            navigate('/recycle');
+            navigate('/recycle', { state: heading });
         }
     };
 
     return (
-        <div className={`${s.recycleCard} ${card.type}Card ${isExpanded ? s.recycleCardExpanded : ''}`}>
-            <div className={s.recycleCardHeader}>
+        <div id={card.heading} className={`${s.recycleCard} ${card.type}Card ${isExpanded ? s.recycleCardExpanded : ''}`}>
+            <div className={s.recycleCardHeader} onClick={() => handleCardClick(card.heading)}>
                 <img src={card.image} alt={card.type} className={s.recycleCardImage} />
                 <div className={s.recycleCardDescriptionWrapper}>
-                    <div className={s.recycleCardDescription} onClick={handleCardClick}>
+                    <div className={s.recycleCardDescription}>
                         <div className={s.recycleCardInfo}>
                             <h3 className={s.recycleCardHeading}>{card.heading}</h3>
                         </div>
                         <RecycleCardButton
-                            onClick={handleCardClick}
+                            onClick={() => handleCardClick(card.heading)}
                             icon={(content && expandable) ? (isExpanded ? IconCross : IconArrow) : IconArrowRight}
                         />
                     </div>
