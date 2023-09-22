741b89003ed34d37c02aaa57357872bd990b6cdf
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2023-09-22 22:52:26 +0300

    feat: add custom select with a dropdown

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,18 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, MouseEvent } from 'react';
+
+import selectDownArrowIcon from 'src/assets/icons/select-down-arrow.svg';
+import selectUpArrowIcon from 'src/assets/icons/select-up-arrow.svg';
+import { useClickOutside } from 'src/hooks/useClickOutside';
 
 import s from  './FeedbackForm.module.scss';
 
 export const FeedbackForm = () => {
+    const dropdownRef = useClickOutside(() => setOpenDropdown(false));
     // Form state control
     const [formData, setFormData] = useState({
         name: '',
         email: '',
+        messageType: 'Отзыв',
         message: ''
     });
     // Message symbols count
     const [count, setCount] = useState(0);
     // Checkbox control
     const [agreed, setAgreed] = useState(false);
+    // Select dropdown control
+    const [openDropdown, setOpenDropdown] = useState(false);
 
     useEffect(() => {
         setCount(formData.message.length);
@@ -25,43 +33,70 @@ export const FeedbackForm = () => {
         });
     };
 
+    const handleSelect = (e: MouseEvent) => {
+        const item = e.target;
+        if (item) {
+            setFormField('messageType', (item as HTMLLIElement).textContent || '');
+        }
+        setOpenDropdown(false);
+    };
+
     return (
         <form className={s.feedbackForm}>
             <h2 className={s.feedbackFormHeading}>Связаться с нами</h2>
             <p className={s.feedbackFormDescription}>Если у вас есть вопросы или предложения — заполните форму ниже</p>
-            <div className={s.feedbackFormField}>
-                <label htmlFor='name'>Имя</label>
-                <input
-                    type='text'
-                    id='name'
-                    name='name'
-                    value={formData.name}
-                    onChange={(e) => setFormField(e.target.name, e.target.value)}
-                />
-            </div>
-            <div className={s.feedbackFormField}>
-                <label htmlFor='email'>E-mail</label>
-                <input
-                    type='email'
-                    id='email'
-                    name='email'
-                    value={formData.email}
-                    onChange={(e) => setFormField(e.target.name, e.target.value)}
-                />
-            </div>
-            <div className={s.feedbackFormField}>
-                <label htmlFor='message'>Сообщение</label>
-                <textarea
-                    id='message'
-                    name='message'
-                    maxLength={300}
-                    value={formData.message}
-                    onChange={(e) => setFormField(e.target.name, e.target.value)}
-                />
-                <div className={s.messageCounter}>{count}/300</div>
+            <div className={s.feedbackFields}>
+                <div className={s.feedbackFormField}>
+                    <label htmlFor='name'>Имя</label>
+                    <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        value={formData.name}
+                        onChange={(e) => setFormField(e.target.name, e.target.value)}
+                    />
+                </div>
+                <div className={s.feedbackFormField}>
+                    <label htmlFor='email'>E-mail</label>
+                    <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        value={formData.email}
+                        onChange={(e) => setFormField(e.target.name, e.target.value)}
+                    />
+                </div>
+                <div className={s.feedbackFormField}>
+                    <label>Тип сообщения</label>
+                    <div ref={dropdownRef}>
+                        <button type='button' className={s.customSelect} onClick={() => setOpenDropdown(!openDropdown)}>
+                            <span>{formData.messageType}</span>
+                            <img src={openDropdown ? selectUpArrowIcon : selectDownArrowIcon} alt='down' />
+                        </button>
+                        {openDropdown &&
+                            <ul className={s.selectDropdown}>
+                                <li className={s.selectItem} onClick={(e) => handleSelect(e)}>Вопрос</li>
+                                <li className={s.selectItem} onClick={(e) => handleSelect(e)}>Предложение</li>
+                                <li className={s.selectItem} onClick={(e) => handleSelect(e)}>Проблема/жалоба</li>
+                                <li className={s.selectItem} onClick={(e) => handleSelect(e)}>Отзыв</li>
+                            </ul>
+                        }
+                    </div>
+                </div>
+                <div className={s.feedbackFormField}>
+                    <label htmlFor='message'>Сообщение</label>
+                    <textarea
+                        id='message'
+                        name='message'
+                        maxLength={500}
+                        value={formData.message}
+                        onChange={(e) => setFormField(e.target.name, e.target.value)}
+                    />
+                    <div className={s.messageCounter}>{count}/500</div>
+                </div>
             </div>
             <div className={s.feedbackFormPolytics}>
-                <div className={`${s.feedbackFormCheckbox}${agreed ? ' ' + s.selected : ''}`} onClick={() => setAgreed(!agreed)}/>
+                <button type='button' className={`${s.feedbackFormCheckbox}${agreed ? ' ' + s.selected : ''}`} onClick={() => setAgreed(!agreed)} />
                 <input type='checkbox' id="agree" checked={agreed} onChange={() => setAgreed(!agreed)} />
                 <span>
                     <label onClick={() => setAgreed(!agreed)}>отправляя сообщение, вы соглашаетесь&nbsp;</label>
