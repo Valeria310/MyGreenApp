2d2c8efc437e9e6f08f4bf4de0020aafe57fc77a
alekseirechitskiy <slowspirit@gmail.com>
2023-09-23 11:47:10 +0300

    added AdminPanel page

---
+++ src/components/adminPanel/AdminPanel/AdminPanel.tsx
@@ -0,0 +1,19 @@
+import CreatePoint from '../CreatePoint';
+import EditPoint from '../EditPoint';
+import LogIn from '../LogIn';
+import ManagePoints from '../ManagePoints';
+import Point from '../Point';
+
+const AdminPanel = () => {
+    return (
+        <>
+            {/* <LogIn /> */}
+            {/* <CreatePoint /> */}
+            {/* <EditPoint /> */}
+            <Point />
+            {/* <ManagePoints /> */}
+        </>
+    );
+};
+
+export default AdminPanel;
