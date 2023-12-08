0b6a95cdbab626a8559bcd07edea3c73c3832f75
alekseirechitskiy <slowspirit@gmail.com>
2023-12-08 11:58:17 +0300

    added PivacyPolicy desktop layout

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -6,6 +6,7 @@ import { Header } from 'src/components/Header/Header';
 import MapSection from 'src/components/MapSection/MapSection';
 import NavSection from 'src/components/NavSection';
 import { ParallaxSection } from 'src/components/Parallax/Parallax';
+import PrivacyPolicy from 'src/components/PrivacyPolicy';
 import RecycleSection from 'src/components/RecycleSection';
 import { Ticker } from 'src/components/Ticker/Ticker';
 import { useFixedContactButton } from 'src/hooks/useFixedContactButton';
@@ -28,21 +29,23 @@ export const Homepage = () => {
     const [ref] = useFixedContactButton();
 
     return (
-        <React.Fragment>
-            <Header />
-            <div className='home-sticky-container'>
-                <ParallaxSection />
-                <NavSection />
-                <RecycleSection expandable={false} />
-                <MapSection />
-                <ContactButton />
-            </div>
-            <section ref={ref} className="ticker">
-                <Ticker />
-            </section>
-            <footer className="footer">
-                <Footer />
-            </footer>
-        </React.Fragment>
+        <PrivacyPolicy />
+
+    // <React.Fragment>
+    //     <Header />
+    //     <div className='home-sticky-container'>
+    //         <ParallaxSection />
+    //         <NavSection />
+    //         <RecycleSection expandable={false} />
+    //         <MapSection />
+    //         <ContactButton />
+    //     </div>
+    //     <section ref={ref} className="ticker">
+    //         <Ticker />
+    //     </section>
+    //     <footer className="footer">
+    //         <Footer />
+    //     </footer>
+    // </React.Fragment>
     );
 };
